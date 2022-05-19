b7d91b7f55475ce471f74e67f515525ae23123f2
forhadmia1 <forhad8613@gmail.com>
2022-05-19 06:53:15 +0600

    post api create successfully

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
@@ -7,6 +8,33 @@ const port = process.env.PORT || 5000;
 //middleware
 app.use(express.json())
 app.use(cors())
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s7c5u.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect()
+        const taskCollection = client.db('todoapp').collection('task');
+
+        // post data
+        app.post('/addTask', async (req, res) => {
+            const data = req.body;
+            const result = await taskCollection.insertOne(data);
+            res.send(result)
+        })
+
+
+    } finally {
+
+    }
+}
+run().catch(console.dir)
+
+
+
 
 app.get('/', (req, res) => {
     res.send('getting started to do app')
